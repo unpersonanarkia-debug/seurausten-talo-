8226179fd7361441b03182b0a68a6683528fd0b0
unpersonanarkia-debug <unpersonanarkia@gmail.com>
2026-01-01 22:02:45 +0200

    Create data-cases.js
    Create data-cases.js added

---
+++ data-cases.js
@@ -0,0 +1,11 @@
+// data-cases.js – Seurausten talon caserakenteet
+// Lisätään tähän uusia esimerkkicaseja
+
+const cases = [
+  {
+    id: "esimerkki-a",
+    title: "Esimerkki A – X-lain uudistus",
+    context: "Kansallinen lainsäädäntöuudistus, jonka tavoitteena oli yksinkertaistaa järjestelmää ja vähentää kustannuksia.",
+    actors: "Lainsäätäjät, ministeriö, etujärjestöt, kansalaiset",
+    slots: {
+      PA
